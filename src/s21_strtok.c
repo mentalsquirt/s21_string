2f02193ceac897ac8a8f22de4f2c7e59652f3614
mentalsquirt <balaban.seva@gmail.com>
2022-12-17 02:23:14 +0700

    refactured

--- src/s21_strtok.c
+++ src/s21_strtok.c
@@ -1,17 +1,15 @@
 #include "s21_string.h"
-#include <string.h>
-#include <stdio.h>
 
 char *olds;
 
 char *s21_strtok(char *str, const char *delim) {
-  char *token;
+  char *token = NULL;
 
   if (str == NULL)
     str = olds;
 
   /* Scan leading delimiters.  */
-  str += s21_strspn (str, delim);
+  str += s21_strspn (str, delim);  // handles possible trailing delims
   if (*str == '\0')
     {
       olds = str;
@@ -23,7 +21,7 @@ char *s21_strtok(char *str, const char *delim) {
   str = s21_strpbrk (token, delim);
   if (str == NULL)
     /* This token finishes the string.  */
-    olds = memchr(token, '\0', 1024);
+    olds = s21_memchr(token, '\0', 1024);`
   else
     {
       /* Terminate the token and make OLDS point past it.  */
@@ -32,36 +30,3 @@ char *s21_strtok(char *str, const char *delim) {
     }
   return token;
 }
-
-size_t s21_strspn(const char *s, const char *accept) {
-  const char *p;
-  const char *a;
-  size_t count = 0;
-
-  for (p = s; *p != '\0'; ++p)
-    {
-      for (a = accept; *a != '\0'; ++a)
-    if (*p == *a)
-      break;
-      if (*a == '\0')
-    return count;
-      else
-    ++count;
-    }
-
-  return count;
-}
-
-char *s21_strpbrk(const char *s, const char *accept) {
-  while (*s != '\0')
-    {
-      const char *a = accept;
-      while (*a != '\0')
-    if (*a++ == *s)
-      return (char *) s;
-      ++s;
-    }
-
-  return NULL;
-}
-
